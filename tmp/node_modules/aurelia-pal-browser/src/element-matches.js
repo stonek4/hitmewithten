ca0cb995029765c6359fd3978f4edb7f2600324e
Kevin Stoner <stonek4rpi@gmail.com>
2017-04-03 09:30:13 -0600

    removed tmp folder

--- tmp/node_modules/aurelia-pal-browser/src/element-matches.js
+++
@@ -1,6 +0,0 @@
-if (Element && !Element.prototype.matches) {
-  let proto = Element.prototype;
-  proto.matches = proto.matchesSelector ||
-    proto.mozMatchesSelector || proto.msMatchesSelector ||
-    proto.oMatchesSelector || proto.webkitMatchesSelector;
-}
